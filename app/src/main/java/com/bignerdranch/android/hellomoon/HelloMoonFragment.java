7cdc5e9c1ca845db8b6057588f41d11f2340dead
apushkar <alexanderpushkar86@gmail.com>
2017-11-05 10:55:04 +0200

    Fix layout. Play/Stop implement.

--- app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.hellomoon;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,20 +9,40 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 /**
+ * class HelloMoonFragment
  * Created by apushkar on 02.11.2017.
  */
 
 public class HelloMoonFragment extends Fragment {
     private Button mPlayButton;
     private Button mStopButton;
+    private AudioPlayer mPlayer = new AudioPlayer();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 
-        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_startButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.play(getActivity());
+            }
+        });
         mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPlayer.stop();
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
     }
 }
